7c9606b0e7811bf72ad37ba465f04122ed9f8e8b
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-08 16:35:27 -0600

    functions, first three "puzzles" and taking damage

--- 2DDungeonGame/main.cpp
+++ 2DDungeonGame/main.cpp
@@ -2,6 +2,48 @@
 #include <string>
 using namespace std;
 
+int puzzle1() {
+	cout << "Puzzle 1! What is 7 + 2?\n";
+	int num;
+	cin >> num;
+	if (num == 9) {
+		cout << "CORRECT\n";
+		return 0;
+	}
+	else {
+		cout << "INCORRECT\n";
+		return 9;
+	}
+}
+
+int puzzle2(int x, int y) {
+	cout << "Puzzle 2! What is " << x << " + " << y << "?\n";
+	int num;
+	cin >> num;
+	if (num == x + y) {
+		cout << "CORRECT\n";
+		return 0;
+	}
+	else {
+		cout << "INCORRECT\n";
+		return 15;
+	}
+}
+
+int puzzle3(string name) {
+	cout << "Puzzle 3! What is your name?\n";
+	string n;
+	cin >> n;
+	if (n == name) {
+		cout << "CORRECT\n";
+		return 0;
+	}
+	else {
+		cout << "INCORRECT\n";
+		return 25;
+	}
+}
+
 void main() {
 	string name;
 	int age;
@@ -15,7 +57,7 @@ void main() {
 
 
 	cout << "Hello world!" << endl;
-	cout << "What is you name?" << endl;
+	cout << "What is your name?" << endl;
 	cin >> name;
 
 	cout << "Welcome to the dungeon " << name << "!" << endl;
@@ -75,7 +117,12 @@ void main() {
 		cout << i << endl;
 	}
 
+	cout << "In the first room stands a tall slender figure in a cloak.\nTotally skull faced. He speaks:\nI will ask you 3 questions with various punishments if you get them wrong...";
+	int dmg = puzzle1() + puzzle2(str, agi) + puzzle3(name);
 
+	hp = hp - dmg;
+
+	cout << "You took " << dmg << " damage!\n " << "HP: " << hp << endl;
 
 	system("pause");
 }
