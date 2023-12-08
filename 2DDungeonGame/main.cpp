b9f81710f09bd0227b2969f5b2d5bc79e161bd94
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-08 14:01:03 -0600

    conditional and math review

--- 2DDungeonGame/main.cpp
+++ 2DDungeonGame/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 void main() {
 	string name;
 	int age;
+	int favNum;
+
 
 	cout << "Hello world!" << endl;
 	cout << "What is you name?" << endl;
@@ -14,6 +16,19 @@ void main() {
 	cout << "How old are you?\n";
 	cin >> age;
 	cout << "So you are " << age << " years old!\n";
+
+	cout << "What is you favorite number?\n";
+	cin >> favNum;
+	cout << "Well you are now stuck in this dungeon for " << favNum << " years!\n";
+
+	int newAge = age + favNum;
+	cout << "It's time to escape before you turn " << newAge << " years old!\n";
+
+	if (newAge >= 80) {
+		cout << "You might not even live to this age!\n";
+	}
+
+
 
 	system("pause");
 }
