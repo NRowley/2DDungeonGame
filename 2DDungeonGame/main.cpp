3bc6db2cb1a7c44debac69fd8b9025f1b33e329a
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-08 14:14:45 -0600

    conditional and while loop for character class setup and stat allocation

--- 2DDungeonGame/main.cpp
+++ 2DDungeonGame/main.cpp
@@ -6,6 +6,12 @@ void main() {
 	string name;
 	int age;
 	int favNum;
+	//hero stats
+	int hp = 0;
+	int str = 0;
+	int def = 0;
+	int agi = 0;
+	bool charSetup = false;
 
 
 	cout << "Hello world!" << endl;
@@ -29,6 +35,40 @@ void main() {
 	}
 
 
+	while (!charSetup) {
+		cout << "What type of character are you?\n";
+		cout << "1 - Slayer\n2 - Adept\n3 - Espion\n";
+		int choice;
+		cin >> choice;
+
+		if (choice == 1) {
+			hp = 100;
+			str = 5;
+			def = 3;
+			agi = 1;
+			charSetup = true;
+		}
+		else if (choice == 2) {
+			hp = 100;
+			str = 3;
+			def = 3;
+			agi = 3;
+			charSetup = true;
+		}
+		else if (choice == 3) {
+			hp = 100;
+			str = 3;
+			def = 1;
+			agi = 5;
+			charSetup = true;
+		}
+		else {
+			cout << "Invalid choice! Try again...\n";
+		}
+	}
+
+	cout << "Stats\n";
+	cout << "HP: " << hp << "\nSTR: " << str << "\nDEF: " << def << "\nAGI: " << agi << endl;
 
 	system("pause");
 }
